0cd0ec4803e859cab9a1ea9b6af800d4392b7c37
MickFrog <107855646+MickFrog@users.noreply.github.com>
2023-02-05 21:48:24 +0300

    Add password validation in js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-// const form = document.querySelector(".userForm");
+const form = document.querySelector(".userForm");
 const userPass = document.querySelector("#userpwd");
 const confirmPass = document.querySelector("#confirmPwd");
+const errorMsg = document.querySelector(".passError")
+
+form.addEventListener('submit', (event) => {
+    if(confirmPass.value != userPass.value) {
+        errorMsg.textContent = "⚠ Passwords must match";
+        event.preventDefault();
+    }
+});
